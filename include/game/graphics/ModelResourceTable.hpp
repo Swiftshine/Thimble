1ff693d11c5a3403da60427208e84a8ee8b83be9
Swift <swiftshineexists@gmail.com>
2026-08-10 11:46:43 -0500

    update ModelResourceTable

--- include/game/graphics/ModelResourceTable.hpp
+++ include/game/graphics/ModelResourceTable.hpp
@@ -1,3 +1,12 @@
 #pragma once
 
-struct ModelResourceTable {};
+#include <types.h>
+
+struct ModelResourceTable {
+    /* 0x00 */ u32 mFlags;
+    /* 0x04 */ const char* mResourceName;
+    /* 0x08 */ u32 mNumSkeletalAnimationIDs;
+    /* 0x0C */ u32* mSkeletalAnimationIDs;
+    /* 0x10 */ u32 mNumSkeletalAnimationNames;
+    /* 0x14 */ const char** mSkeletalAnimationNames;
+};
