c38acfc24927fea9b67bcfef07fe017f9600d696
Swift <swiftshineexists@gmail.com>
2026-08-10 00:28:07 -0500

    update YsModelControl2 header

--- include/game/graphics/YsModelControl2.hpp
+++ include/game/graphics/YsModelControl2.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <gfl/gflMath.hpp>
+#include <gfl/gflModel.hpp>
 #include <ghs_stl/string.hpp>
 #include <graphics/FlfMdlDraw.hpp>
-#include <graphics/ResourceList.hpp>
+#include <graphics/ModelResourceTable.hpp>
 #include <types.h>
 
 class YsModelControl2 {
@@ -14,21 +15,32 @@ public:
     virtual ~YsModelControl2();
 
     void Reset();
-    void Init(FlfMdlDraw* pFlfMdlDraw, const char* pArchiveName, const char* pBFRESName, ResourceList* pResourceList,
-              u32 flags, u32 arg6);
+    void Init(FlfMdlDraw* pFlfMdlDraw, const char* pArchiveName, const char* pBFRESName,
+              ModelResourceTable* pResourceList, u32 flags, u32 arg6);
     void UpdateMatrix(const gfl::Matrix34& rMatrix);
+    void PlaySkeletalAnimation(u32 id);
+    float GetUpdateRate() const;
+    void SetUpdateRate(float rate);
+    void UpdateFrame(float step);
+    float GetCurrentFrame() const;
+    float GetEndFrame() const;
+    bool IsAnimationDone() const;
+    // it's only used in a couple places so it's difficult to determine *how*
+    // to use this
+    void EnableAnimationReaction(bool react);
+    bool HasAnimation() const;
 
 public:
-    /* 0x00 */ u32 m_0;
+    /* 0x00 */ u32 mFlags;
     /* 0x04 */ ghs::std::string mArchivePath;
     /* 0x24 */ ghs::std::string mResourceName;
     /* 0x44 */ int m_44;
-    /* 0x48 */ u32 m_48;
-    /* 0x4C */ void* m_4C;
-    /* 0x50 */ u32 m_50;
-    /* 0x54 */ int m_54;
-    /* 0x58 */ u32 m_58;
-    /* 0x5C */ float m_5C;
+    /* 0x48 */ FlfMdlDraw* mFlfMdlDraw;
+    /* 0x4C */ gfl::Model* mModel;
+    /* 0x50 */ ModelResourceTable* mModelResourceTable;
+    /* 0x54 */ u32 mCurrentAnimIndex;
+    /* 0x58 */ u32 mCurrentSkeletalAnimIndex;
+    /* 0x5C */ float mUpdateRate;
     /* 0x60 */ gfl::Matrix34 m_60;
     /* 0x90 */ gfl::Matrix34 m_90;
     /* 0xC0 */ u32 m_C0;
--- include/types.h
+++ include/types.h
@@ -2,6 +2,9 @@
 #include <cstddef>
 #include <cstdint>
 #include <telkin/Preprocessor.h>
+
+using placeholder_t = int;
+
 using u8 = uint8_t;
 using s8 = int8_t;
 using u16 = uint16_t;
